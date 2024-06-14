6aa1dc7bf054d5ed6a78e362c28d6a8c7b53092f
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2024-06-14 10:21:06 +0100

    Remove unused code

--- src/main/scala/synonyms/thesaurus/package.scala
+++ src/main/scala/synonyms/thesaurus/package.scala
@@ -21,46 +21,8 @@ final case class Entry(
       AreSynonyms(word, check, partOfSpeech, definition, example, thesaurusName)
     else NotSynonyms(word, check)
 
-object Entry:
-  def synonymsByLength(entries: List[Entry]): List[(Int, List[String])] =
-    entries
-      .flatMap(_.synonyms)
-      .distinct
-      .groupBy(_.count(Character.isAlphabetic))
-      .toList
-      .map { case (k, v) => k -> v.sorted }
-      .sorted
-
 final case class SynonymsByLength private (length: Int, synonyms: List[String])
 
-//class Outer[+A](x: A) {
-//  class Inner[B >: A](y: B)
-//}
-//
-//object Foo:
-//  abstract class Animal:
-//    def name: String
-//
-//  case class Cat(name: String) extends Animal
-//  case class Dog(name: String) extends Animal
-//
-//  class Pets[+A](val pet: A) {
-//    def add(pet2: A): String = "done"
-//  }
-//  val cats               = new Pets[Cat](Cat("Felix"))
-//  val pets: Pets[Animal] = cats
-//  pets.add(Dog("Fido"))
-//
-//  abstract class Serializer[-A]:
-//    def serialize(a: A): String
-//
-//  val animalSerializer: Serializer[Animal] = new Serializer[Animal]():
-//    def serialize(animal: Animal): String = s"""{ "name": "${animal.name}" }"""
-//  animalSerializer.serialize(Cat("Felix"))
-//
-//  val catSerializer: Serializer[Cat] = animalSerializer
-//  catSerializer.serialize(Cat("Felix"))
-//
 object SynonymsByLength:
   given Show[SynonymsByLength] with
     def show(sbl: SynonymsByLength): String =
