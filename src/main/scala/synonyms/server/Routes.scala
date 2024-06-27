b1a8496666f6c52adf779bfce280e9833141208f
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2024-06-27 17:25:36 +0100

    Support Accept header

--- src/main/scala/synonyms/server/Routes.scala
+++ src/main/scala/synonyms/server/Routes.scala
@@ -4,16 +4,19 @@ import cats.data.Validated.*
 import cats.data.ValidatedNel
 import cats.effect.*
 import cats.syntax.apply.*
+import cats.syntax.show.*
 import cats.syntax.validated.*
 import io.circe.*
 import io.circe.generic.auto.*
+import io.circe.syntax.*
 import org.http4s.*
-import org.http4s.circe.CirceEntityEncoder.*
+import org.http4s.circe.*
 import org.http4s.dsl.io.*
+import org.http4s.headers.Accept
 import synonyms.server.QueryParamDecoders.given
 import synonyms.thesaurus.*
 import synonyms.thesaurus.algebra.Client
-import synonyms.thesaurus.response.SynonymsByLength
+import synonyms.thesaurus.response.{Result, SynonymsByLength}
 
 object Routes:
   object ThesaurusParamMatcher
@@ -47,29 +50,56 @@ object Routes:
           ).invalidNel
       }
 
-  val service: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root / "synonyms" / WordVar(word) :? ThesaurusParamMatcher(
-          thesaurusesValidated
-        ) =>
-      thesaurusesValidated.withDefault match
-        case Valid(thesauruses) =>
-          Service()
-            .getEntries2(word, thesauruses)
-            .map(SynonymsByLength.fromEntries)
-            .foldF(_ => InternalServerError(), Ok(_))
-        case Invalid(e) => BadRequest(e.map(_.sanitized))
+  extension (acceptHeader: Accept)
+    def satisfiedBy(range: MediaRange): Boolean =
+      acceptHeader.values.exists(_.mediaRange.satisfiedBy(range))
+    def isJson: Boolean = acceptHeader.satisfiedBy(MediaType.application.json)
+    def isText: Boolean = acceptHeader.satisfiedBy(MediaType.text.plain)
 
-    case GET -> Root / "synonyms" :? WordsMatcher(
+  val service: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case req @ GET -> Root / "synonyms" / WordVar(
+          word
+        ) :? ThesaurusParamMatcher(
+          thesaurusesValidated: ValidatedNel[ParseFailure, List[Client[IO]]]
+        ) =>
+      def getEntries[A](f: List[SynonymsByLength] => A)(using
+          EntityEncoder[IO, A]
+      ): IO[Response[IO]] =
+        thesaurusesValidated.withDefault match
+          case Valid(thesauruses) =>
+            Service()
+              .getEntries2(word, thesauruses)
+              .map(SynonymsByLength.fromEntries)
+              .foldF(_ => InternalServerError(), syns => Ok(f(syns)))
+          case Invalid(e) => BadRequest(e.map(_.sanitized).asJson)
+
+      req.headers.get[Accept] match
+        case Some(value) if value.isJson => getEntries(_.asJson)
+        case Some(value) if value.isText => getEntries(_.show)
+        case Some(value) => BadRequest(s"Unsupported Accept header: $value")
+        case None        => getEntries(_.asJson)
+
+    case req @ GET -> Root / "synonyms" :? WordsMatcher(
           words
         ) +& ThesaurusParamMatcher(thesaurusesValidated) =>
-      val validated = (thesaurusesValidated.withDefault, words.toTuple2).mapN {
-        case (thesauruses, (first, second)) =>
-          Service().checkSynonyms2(first, second, thesauruses)
-      }
-      validated match
-        case Valid(result) => result.foldF(_ => InternalServerError(), Ok(_))
-        case Invalid(e)    => BadRequest(e.map(_.sanitized))
+      def checkSynonyms[F[_], A](f: Result => A)(using EntityEncoder[F, A]) =
+        val validated =
+          (thesaurusesValidated.withDefault, words.toTuple2).mapN {
+            case (thesauruses, (first, second)) =>
+              Service().checkSynonyms2(first, second, thesauruses)
+          }
+        validated match
+          case Valid(result) =>
+            result.foldF(_ => InternalServerError(), f => Ok(f.asJson))
+          case Invalid(e) => BadRequest(e.map(_.sanitized).asJson)
+
+      req.headers.get[Accept] match
+        case Some(value) if value.isJson => checkSynonyms(_.asJson)
+        case Some(value) if value.isText => checkSynonyms(_.show)
+        case Some(value) => BadRequest(s"Unsupported Accept header: $value")
+        case None        => checkSynonyms(_.asJson)
   }
 
-object WordVar:
-  def unapply(str: String): Option[Word] = Option.when(str.nonEmpty)(Word(str))
+  object WordVar:
+    def unapply(str: String): Option[Word] =
+      Option.when(str.nonEmpty)(Word(str))
