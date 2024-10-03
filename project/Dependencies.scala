111c34845dcbc11fde091d5621e24424d4147c12
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2024-10-03 14:27:53 +0000

    Update fs2-core, fs2-io to 3.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsEffect            = "3.5.4"
     val circe                 = "0.14.10"
     val decline               = "2.4.1"
-    val fs2                   = "3.10.2"
+    val fs2                   = "3.11.0"
     val fs2data               = "1.11.1"
     val http4s                = "0.23.28"
     val http4sBlaze           = "0.23.16"
