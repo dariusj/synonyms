a4efe3ee601c99f220f09a721afe9c6e75a73f7c
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2024-10-03 14:28:00 +0000

    Update circe-core, circe-generic, ... to 0.14.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object V {
     val cats                  = "2.12.0"
     val catsEffect            = "3.5.4"
-    val circe                 = "0.14.9"
+    val circe                 = "0.14.10"
     val decline               = "2.4.1"
     val fs2                   = "3.10.2"
     val fs2data               = "1.11.1"
