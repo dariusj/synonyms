bce25fa1d398adf9c188a6eddff473d351998a5c
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2025-01-11 18:47:52 +0000

    Update munit to 1.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val log4cats              = "2.7.0"
     val logback               = "1.5.16"
     val monocle               = "3.3.0"
-    val munit                 = "1.0.3"
+    val munit                 = "1.0.4"
     val munitCatsEffect       = "2.0.0"
     val munitScalacheck       = "1.0.0"
     val scalaScraper          = "3.1.2"
