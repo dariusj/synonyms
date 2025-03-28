8818bd607477bbe2a051fb022c201e36bcc6d458
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2025-03-28 21:20:52 +0000

    Update cats-effect to 3.6.0 (#58)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object V {
     val cats                  = "2.13.0"
-    val catsEffect            = "3.5.7"
+    val catsEffect            = "3.6.0"
     val catsLaws              = "2.13.0"
     val circe                 = "0.14.12"
     val decline               = "2.5.0"
