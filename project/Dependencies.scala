080c8514494a7c31f10fc3f821e893951c667a1f
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2025-11-09 10:31:00 +0000

    Update iron to 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val fs2data               = "1.12.0"
     val http4s                = "0.23.33"
     val http4sBlaze           = "0.23.17"
-    val iron                  = "3.2.0"
+    val iron                  = "3.2.1"
     val log4cats              = "2.7.1"
     val logback               = "1.5.20"
     val monocle               = "3.3.0"
