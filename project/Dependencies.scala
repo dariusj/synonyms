debb2e0e64318c4dfa25558170cc8267fd9e2438
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2026-09-13 13:39:23 +0000

    Update munit-scalacheck to 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val monocle               = "3.3.0"
     val munit                 = "1.3.1"
     val munitCatsEffect       = "2.2.0"
-    val munitScalacheck       = "1.3.0"
+    val munitScalacheck       = "1.3.1"
     val scalaScraper          = "3.2.0"
     val scalacheckEffectMunit = "2.1.0"
   }
